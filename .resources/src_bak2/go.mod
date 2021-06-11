0bd36dff5a79a31f9d5f3389fc5d5fd3530cbdd0
Fabijan Bajo <bajo09@gmail.com>
2021-06-12 00:46:05 +0200

    remove folders

--- .resources/src_bak2/go.mod
+++
@@ -1,8 +0,0 @@
-module service
-
-go 1.15
-
-require (
-	github.com/caarlos0/env v3.5.0+incompatible
-	github.com/caarlos0/env/v6 v6.5.0
-)
--- .resources/src_bak2/message.go
+++
@@ -1,7 +0,0 @@
-package main
-
-// Message represents a message data transfer object
-type Message struct {
-	Title string `json:"title"`
-	Body  string `json:"body"`
-}
